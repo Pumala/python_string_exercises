7b255ee54b6e4b0cd6315a6041f5429a0a62fc80
Carolyn Lam <caro_lyn_@hotmail.com>
2016-09-24 12:54:24 -0400

    add new file: capitalize_a_string.py

---
+++ capitalize_a_string.py
@@ -0,0 +1,6 @@
+print "Capitalize a String Exercise"
+# prompt user for string and return the user's string capitalized
+
+user_string = raw_input("Enter a word or sentence: ")
+
+print user_string[0].upper() + user_string[1:]
--- uppercase_a_string.py
+++ uppercase_a_string.py
@@ -1,3 +1,6 @@
-user_string = raw_input("Enter a string: ")
+print "Uppercase a String Exercise"
+# prompt user for string and return the user's string in uppercase letters
+
+user_string = raw_input("Enter a word or sentence: ")
 
 print user_string.upper()

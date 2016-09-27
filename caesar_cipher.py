2379ae6070ec354615371479c695f17d00d6203a
Carolyn Lam <caro_lyn_@hotmail.com>
2016-09-26 23:09:39 -0400

    update caesar_decipher.py

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -22,24 +22,3 @@ for letter in user_input:
         new_message += " "
 
 print new_message
-
-
-
-
-# user_input = (raw_input("Enter a word or sentence: ")).lower()
-#
-#
-# alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
-#             "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
-#             ]
-# new_message = ""
-#
-# for letter in user_input:
-#     for alpha in alphabet:
-#         if letter == alpha:
-#             index = (alphabet.index(alpha)) + 1
-#             new_message += alphabet[index]
-#     if letter == " ":
-#         new_message += " "
-#
-# print new_message
--- caesar_decipher.py
+++ caesar_decipher.py
@@ -1,19 +1,23 @@
 print "Caesar Decipher Exercise"
 # Given a string that has been "ciphered", return the deciphered form
 
-ciphered_text = "xibu ep zpv uijol bcpvu uif tlz"
+ciphered_text = "abobql pbhyq haqrefgnaq jul gur ghegyr pyvzorq bagb gur juvzfvpny qnapvat onex gung ynl arneyl sbetbggra va gur fhzzre urng"
+shifted = 13
 
 alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
             "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
             ]
-deciphered_text = ""
+new_message = ""
 
-for letter in ciphered_text:
-    for alpha in alphabet:
-        if letter == alpha:
-            index = (alphabet.index(alpha)) - 1
-            deciphered_text += alphabet[index]
-    if letter == " ":
-        deciphered_text += " "
+for char in ciphered_text:
+    for letter in alphabet:
+        if char == letter:
+            index = (alphabet.index(letter)) + shifted
+            if index > 25:
+                neg_index = (index - 25)
+                index = neg_index - 1
+            new_message += alphabet[index]
+    if char == " ":
+        new_message += " "
 
-print deciphered_text
+print new_message

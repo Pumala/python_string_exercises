f1e945cf1b0f7f6e7eff4712826c197ddffd976a
Carolyn Lam <caro_lyn_@hotmail.com>
2016-09-26 22:41:26 -0400

    add a shifter variable to caesar_cipher.py

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -3,6 +3,7 @@ print "Caesar Cipher Exercise"
 # What is Caesar Cipher? http://practicalcryptography.com/ciphers/caesar-cipher/
 
 user_input = (raw_input("Enter a word or sentence: ")).lower()
+shifted = int(raw_input("How many times would you like to shift the letter? "))
 
 alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
             "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
@@ -12,9 +13,33 @@ new_message = ""
 for letter in user_input:
     for alpha in alphabet:
         if letter == alpha:
-            index = (alphabet.index(alpha)) + 1
+            index = (alphabet.index(alpha)) + shifted
+            if index > 25:
+                neg_index = (index - 25)
+                index = neg_index - 1
             new_message += alphabet[index]
     if letter == " ":
         new_message += " "
 
 print new_message
+
+
+
+
+# user_input = (raw_input("Enter a word or sentence: ")).lower()
+#
+#
+# alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
+#             "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
+#             ]
+# new_message = ""
+#
+# for letter in user_input:
+#     for alpha in alphabet:
+#         if letter == alpha:
+#             index = (alphabet.index(alpha)) + 1
+#             new_message += alphabet[index]
+#     if letter == " ":
+#         new_message += " "
+#
+# print new_message
